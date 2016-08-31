b3207d5a94c1362ae884f3d20a20471bfb6a416c
argoc <agarripoli@olympic.edu>
2016-08-30 21:46:36 -0700

    database started

---
+++ source/App_Code/PhoneBookEntry.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+/// <summary>
+/// PhoneBookEntry matches a row in the PhoneBookDB
+/// </summary>
+public class PhoneBookEntry
+{
+    public PhoneBookEntry(dynamic row)
+    {
+        Id = row.Id;
+        Name = row.Name;
+        PhoneNumber = row.PhoneNumber;
+        Email = row.Email;
+        TwitterHandle = row.TwitterHandle;
+    }
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int PhoneNumber { get; set; }
+    public string Email { get; set; }
+    public string TwitterHandle { get; set; }
+}
